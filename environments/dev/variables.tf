5cbc4a34698590eb86f977f80b3fdeb5a8c369c3
tobermeier23 <tobermeier23@gmail.com>
2025-06-10 13:47:54 -0500

    Add files via upload

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,3 +2,19 @@ variable "project_number" {
     type = string
     default = "236606652434"
 }
+
+variable "project_id" {
+    type = string
+    default = "ninth-sol-462415-k7"
+}
+
+variable "region" {
+    type = string
+    default = "us-central1"
+}
+
+variable "zone" {
+    type = string
+    default = "us-central1-a"
+}
+
