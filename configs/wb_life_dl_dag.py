bd731e642fc5025803bcd8d4df4760859b660c5c
tobermeier23 <tobermeier23@gmail.com>
2025-06-10 11:57:29 -0500

    Add files via upload

--- configs/wb_life_dl_dag.py
+++ configs/wb_life_dl_dag.py
@@ -26,17 +26,12 @@ https://cloud.google.com/dataflow/docs/resources/locations
 Function (.js), the input file (.txt), and the JSON schema (.json).
 """
 
+import urllib.request
 import datetime
 
 from airflow import models
-from airflow.providers.google.cloud.operators.dataflow import DataflowTemplatedJobStartOperator
+from airflow.decorators import task
 from airflow.utils.dates import days_ago
-
-bucket_path = models.Variable.get("bucket_path")
-project_id = models.Variable.get("project_id")
-gce_zone = models.Variable.get("gce_zone")
-location = models.Variable.get("location")
-
 
 default_args = {
     # Tell airflow to start one day ago, so that it runs as soon as you upload it
@@ -46,7 +41,8 @@ default_args = {
         # Set to your zone
         "zone": "us-central1-a",
         # This is a subfolder for storing temporary files, like the staged pipeline job.
-        "tempLocation": "gs://etl-task-files/tmp/",
+        "uri": "https://api.worldbank.org/v2/en/indicator/SP.DYN.LE00.IN?downloadformat=csv",
+        "target_path": "gs://wb-le-task-files",
         "serviceAccountEmail": "etlpipeline@ninth-sol-462415-k7.iam.gserviceaccount.com",
     },
 }
@@ -56,32 +52,14 @@ default_args = {
 # DAG object.
 with models.DAG(
     # The id you will see in the DAG airflow page
-    "etl_task_dataflow_dag",
+    "wb_life_dl_dag",
     default_args=default_args,
     # The interval with which to schedule the DAG
     schedule_interval=datetime.timedelta(days=1),  # Override to match your needs
 ) as dag:
 
-    start_template_job = DataflowTemplatedJobStartOperator(
-        # The task id of your job
-        task_id="dataflow_operator_transform_csv_to_bq",
-        # The name of the template that you're using.
-        # Below is a list of all the templates you can use.
-        # For versions in non-production environments, use the subfolder 'latest'
-        # https://cloud.google.com/dataflow/docs/guides/templates/provided-batch#gcstexttobigquery
-        template="gs://dataflow-templates-us-central1/latest/GCS_CSV_to_BigQuery",
-        # Use the link above to specify the correct parameters for your template.
-        location="us-central1",
-        parameters={
-            #"javascriptTextTransformFunctionName": "etltasktransformCSVtoJSON",
-            "schemaJSONPath": "gs://etl-task-files/ETLTaskjsonSchema.json",
-            #"javascriptTextTransformGcsPath": "gs://etl-task-files/etltasktransformCSVtoJSON.js",
-            "inputFilePattern": "gs://etl-task-files/ETLTaskinputFile.csv",
-            "outputTable": "ninth-sol-462415-k7:icsa.icsa",
-            "bigQueryLoadingTemporaryDirectory": "gs://etl-task-files/tmp/",
-            "badRecordsOutputTable": "ninth-sol-462415-k7:icsa.bad_icsa",
-            "delimiter": ",",
-            "containsHeaders": "true",
-            "csvFormat": "Default",
-        },
-    )
+    @task
+    def download_file(uri, target_path):
+        with urllib.request.urlopen(uri) as file:
+            with open(target_path, "wt") as new_file:
+                new_file.write(file.read())
