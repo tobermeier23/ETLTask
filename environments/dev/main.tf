8ee0f57a1ca76ab1cbf5ca228fbae09251c37c36
tobermeier23 <tobermeier23@gmail.com>
2025-06-11 16:04:34 -0500

    Add files via upload

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -43,18 +43,21 @@ resource "google_cloud_run_v2_service" "default" {
 
 resource "google_storage_bucket" "bucket" {
   name     = "fred-run-source-location"  # Every bucket name must be globally unique
+  project = var.project_id
   location = "US"
   uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket_object" "object" {
   name   = "fred-main.py"
+  project = var.project_id
   bucket = google_storage_bucket.bucket.name
   source = "${path.root}/files/fred-main.py"  # Add path to the zipped function source code
 }
 
 resource "google_service_account" "cloudbuild_service_account" {
   account_id = "build-sa"
+  project = var.project_id
 }
 
 resource "google_project_iam_member" "act_as" {
