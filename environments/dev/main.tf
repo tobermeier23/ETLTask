71f8d4e2ef109a494d3e8ade6ce8df8fe487e8ff
tobermeier23 <tobermeier23@gmail.com>
2025-06-11 11:06:34 -0500

    Add files via upload

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -164,21 +164,21 @@ resource "google_storage_bucket" "world_bank_pipeline_files" {
 
 resource "google_storage_bucket_object" "json_schema" {
   name       = "jsonSchema.json"
-  source     = "ETLTaskjsonSchema.json"
+  source     = "~/ETLTaskjsonSchema.json"
   bucket     = google_storage_bucket.pipeline_files.name
   depends_on = [google_storage_bucket.pipeline_files]
 }
 
 resource "google_storage_bucket_object" "bad_json_schema" {
   name       = "BadjsonSchema.json"
-  source     = "BadETLTaskjsonSchema.json"
+  source     = "~/BadETLTaskjsonSchema.json"
   bucket     = google_storage_bucket.pipeline_files.name
   depends_on = [google_storage_bucket.pipeline_files]
 }
 
 resource "google_storage_bucket_object" "input_file" {
   name       = "icnsa_data.csv"
-  source     = "ETLTaskinputFile.csv"
+  source     = "~/ETLTaskinputFile.csv"
   bucket     = google_storage_bucket.pipeline_files.name
   depends_on = [google_storage_bucket.pipeline_files]
 }
