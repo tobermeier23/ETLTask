15022544dc37c9f197f29a27d224af72ceced415
tobermeier23 <tobermeier23@gmail.com>
2025-06-11 15:56:23 -0500

    Add files via upload

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -58,13 +58,13 @@ resource "google_service_account" "cloudbuild_service_account" {
 }
 
 resource "google_project_iam_member" "act_as" {
-  project = data.google_project.project.project_id
+  project = var.project_id
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
 
 resource "google_project_iam_member" "logs_writer" {
-  project = data.google_project.project.project_id
+  project = var.project_id
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
