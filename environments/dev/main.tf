ad2015f9386f5b718c14968442b780452a2833ca
tobermeier23 <tobermeier23@gmail.com>
2025-06-13 09:20:00 -0500

    Add files via upload

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,85 @@
+resource "google_cloud_run_v2_service" "default" {
+  project = var.project_id
+  name     = "fred-service"
+  location = "us-central1"
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-central1-docker.pkg.dev/ninth-sol-462415-k7/cloud-run-source-deploy/fred-download"
+      base_image_uri = "us-central1-docker.pkg.dev/serverless-runtimes/google-22-full/runtimes/python313"
+      volume_mounts {
+        name = "gcs-1"
+        mount_path = "/fredfiles"
+      }
+    }
+
+    volumes {
+      name = "gcs-1"
+      gcs {
+        bucket = "fred-ninth-sol-462415-k7-files"
+        read_only = false
+      }
+    }
+  }
+  build_config {
+    #source_location = "gs://${google_storage_bucket.bucket.name}/${google_storage_bucket_object.object.name}"
+    source_location = "gs://fred-run-source-location/fred-main.zip"
+    function_target = "hello_http"
+    image_uri = "us-central1-docker.pkg.dev/ninth-sol-462415-k7/cloud-run-source-deploy/fred-download"
+    base_image = "us-central1-docker.pkg.dev/serverless-runtimes/google-22-full/runtimes/python313"
+    enable_automatic_updates = true
+    #worker_pool = "worker-pool"
+    service_account = google_service_account.cloudbuild_service_account.id
+  }
+  depends_on = [
+    google_project_iam_member.act_as,
+    google_project_iam_member.logs_writer
+  ]
+}
+
+#data "google_project" "project" {
+#}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "fred-run-source-location"  # Every bucket name must be globally unique
+  project = var.project_id
+  location = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "fred-main.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "${path.root}/files/fred-main.zip"  # Add path to the zipped function source code
+}
+
+resource "google_storage_bucket" "pipeline_files" {
+  project       = var.project_number
+  name          = "fred-${var.project_id}-files"
+  location      = "US"
+  force_destroy = true
+  depends_on    = [google_project_service.all]
+}
+
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "build-sa"
+  project = var.project_id
+}
+
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
 variable "gcp_service_list" {
   description = "The list of apis necessary for the project"
   type        = list(string)
@@ -19,180 +101,9 @@ resource "google_project_service" "all" {
 }
 
 resource "google_service_account" "etl" {
-  account_id   = "etlpipeline"
+  account_id   = "etlpipelinetask"
   display_name = "ETL SA"
   description  = "user-managed service account for Composer and Dataflow"
   project = var.project_id
   depends_on = [google_project_service.all]
-}
-
-resource "google_project_iam_member" "allbuild" {
-  project    = var.project_id
-  for_each   = toset(var.build_roles_list)
-  role       = each.key
-  member     = "serviceAccount:${google_service_account.etl.email}"
-  depends_on = [google_project_service.all,google_service_account.etl]
-}
-
-resource "google_project_iam_member" "composerAgent" {
-  project    = var.project_id
-  role       = "roles/composer.ServiceAgentV2Ext"
-  member     = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
-  depends_on = [google_project_service.all]
-}
-
-# Create Core Composer environment
-resource "google_composer_environment" "fred" {
-  project   = var.project_id
-  name      = "fred"
-  region    = var.region
-  config {
-
-    software_config {
-      image_version = "composer-3-airflow-2.10.5"
-      env_variables = {
-        AIRFLOW_VAR_PROJECT_ID  = var.project_id
-        AIRFLOW_VAR_GCE_ZONE    = var.zone
-        AIRFLOW_VAR_BUCKET_PATH = "gs://fred-${var.project_id}-files"
-      }
-    }
-    node_config {
-      service_account = google_service_account.etl.name
-    }
-  }
-  depends_on = [google_project_service.all, google_service_account.etl, google_project_iam_member.allbuild, google_project_iam_member.composerAgent]
-}
-
-# Create CI/CD Composer environment
-resource "google_composer_environment" "world-bank" {
-  project   = var.project_id
-  name      = "world-bank"
-  region    = var.region
-  config {
-
-    software_config {
-      image_version = "composer-3-airflow-2.10.5"
-      env_variables = {
-        AIRFLOW_VAR_PROJECT_ID  = var.project_id
-        AIRFLOW_VAR_GCE_ZONE    = var.zone
-        AIRFLOW_VAR_BUCKET_PATH = "gs://world-bank-${var.project_id}-files"
-      }
-    }
-    node_config {
-      service_account = google_service_account.etl.name
-    }
-  }
-  depends_on = [google_project_service.all, google_service_account.etl, google_project_iam_member.allbuild, google_project_iam_member.composerAgent]
-}
-
-resource "google_bigquery_dataset" "icnsa_dataset" {
-  project    = var.project_id
-  dataset_id = "icnsa"
-  location   = "US"
-  depends_on = [google_project_service.all]
-}
-
-resource "google_bigquery_dataset" "world_bank_dataset" {
-  project    = var.project_id
-  dataset_id = "world_bank_life_expectancy"
-  location   = "US"
-  depends_on = [google_project_service.all]
-}
-
-resource "google_bigquery_table" "icnsa_table" {
-  project    = var.project_id
-  dataset_id = google_bigquery_dataset.icnsa_dataset.dataset_id
-  table_id   = "icnsa"
-  deletion_protection = false
-
-  schema     = <<EOF
-[
-  {
-    "name": "observation_date",
-    "type": "DATE",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "ICSA",
-    "type": "INTEGER",
-    "mode": "REQUIRED"
-  }  
-]
-EOF
-  depends_on = [google_bigquery_dataset.icnsa_dataset]
-}
-
-resource "google_bigquery_table" "icnsa_bad_table" {
-  project    = var.project_id
-  dataset_id = google_bigquery_dataset.icnsa_dataset.dataset_id
-  table_id   = "bad_icnsa"
-  deletion_protection = false
-
-  schema     = <<EOF
-[
-  {
-    "name": "observation_date",
-    "type": "DATE",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "ICSA",
-    "type": "INTEGER",
-    "mode": "REQUIRED"
-  }  
-]
-EOF
-  depends_on = [google_bigquery_dataset.icnsa_dataset]
-}
-
-# Create Cloud Storage bucket and add files
-resource "google_storage_bucket" "pipeline_files" {
-  project       = var.project_number
-  name          = "fred-${var.project_id}-files"
-  location      = "US"
-  force_destroy = true
-  depends_on    = [google_project_service.all]
-}
-
-resource "google_storage_bucket" "world_bank_pipeline_files" {
-  project       = var.project_number
-  name          = "world-bank-${var.project_id}-files"
-  location      = "US"
-  force_destroy = true
-  depends_on    = [google_project_service.all]
-}
-
-resource "google_storage_bucket_object" "json_schema" {
-  name       = "jsonSchema.json"
-  source     = "files/ETLTaskjsonSchema.json"
-  bucket     = google_storage_bucket.pipeline_files.name
-  depends_on = [google_storage_bucket.pipeline_files]
-}
-
-resource "google_storage_bucket_object" "bad_json_schema" {
-  name       = "BadjsonSchema.json"
-  source     = "files/BadETLTaskjsonSchema.json"
-  bucket     = google_storage_bucket.pipeline_files.name
-  depends_on = [google_storage_bucket.pipeline_files]
-}
-
-resource "google_storage_bucket_object" "input_file" {
-  name       = "icnsa_data.csv"
-  source     = "files/ETLTaskinputFile.csv"
-  bucket     = google_storage_bucket.pipeline_files.name
-  depends_on = [google_storage_bucket.pipeline_files]
-}
-
-data "google_composer_environment" "example" {
-  project    = var.project_id
-  region     = var.region
-  name       = google_composer_environment.fred.name
-  depends_on = [google_composer_environment.fred]
-}
-
-resource "google_storage_bucket_object" "dag_file" {
-  name       = "dags/composer-dataflow-dag.py"
-  source     = "${path.module}/files/composer-dataflow-dag.py"
-  bucket     = replace(replace(data.google_composer_environment.example.config.0.dag_gcs_prefix, "gs://", ""),"/dags","")
-  depends_on = [google_composer_environment.fred, google_storage_bucket.pipeline_files, google_bigquery_table.icnsa_table]
 }
